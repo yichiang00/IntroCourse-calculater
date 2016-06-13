dcc4957db5328c6bb41a27f4fc56a9def4400bf6
Yi Chiang and rudd vetsper <yi+rudd@>
2016-06-13 11:51:04 -0700

    add mi file

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var bmi =function(weight,height){
+return weight/(height*height);
+};
+var weight=parseInt(prompt("Your Weight in Kg!!!?????"));
+var height=parseFloat(prompt("What is your height in meter?"));
+
+alert(bmi(weight,height));
